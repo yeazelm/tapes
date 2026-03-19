103c4a716e1f41b0ea9f2dd6d81245cb983ceb69
John McBride <john@papercompute.com>
2026-03-19 09:37:13 -0600

    🧹 chore: use Dagger 0.20.1 (#162)
    Signed-off-by: John McBride <john@papercompute.com>

--- .dagger/image.go
+++ .dagger/image.go
@@ -14,6 +14,46 @@ const (
 	imageName  = "tapes"
 )
 
+// BuildImages returns just in time images, ready for publishing or exporting to host.
+func (t *Tapes) BuildImages(
+	ctx context.Context,
+
+	// Version string for ldflags
+	version string,
+
+	// Git commit SHA for ldflags
+	commit string,
+) []*dagger.Container {
+	buildtime := time.Now()
+	ldflags := strings.Join([]string{
+		"-s",
+		"-w",
+		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Version=%s'", version),
+		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Sha=%s'", commit),
+		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Buildtime=%s'", buildtime),
+	}, " ")
+
+	amd64 := t.Source.DockerBuild(dagger.DirectoryDockerBuildOpts{
+		Dockerfile: dockerfile,
+		Platform:   "linux/amd64",
+		BuildArgs: []dagger.BuildArg{
+			{Name: "LDFLAGS", Value: ldflags},
+		},
+	})
+	arm64 := t.Source.DockerBuild(dagger.DirectoryDockerBuildOpts{
+		Dockerfile: dockerfile,
+		Platform:   "linux/arm64",
+		BuildArgs: []dagger.BuildArg{
+			{Name: "LDFLAGS", Value: ldflags},
+		},
+	})
+
+	return []*dagger.Container{
+		amd64,
+		arm64,
+	}
+}
+
 // BuildPushImage builds a multi-arch image for tapes
 // using the existing Dockerfile and publishes to the provided registry.
 //
@@ -34,37 +74,14 @@ func (t *Tapes) BuildPushImage(
 	// Git commit SHA for ldflags
 	commit string,
 ) ([]string, error) {
-	buildtime := time.Now()
-	ldflags := strings.Join([]string{
-		"-s",
-		"-w",
-		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Version=%s'", version),
-		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Sha=%s'", commit),
-		fmt.Sprintf("-X 'github.com/papercomputeco/tapes/pkg/utils.Buildtime=%s'", buildtime),
-	}, " ")
-
 	published := []string{}
-
-	amd64 := t.Source.DockerBuild(dagger.DirectoryDockerBuildOpts{
-		Dockerfile: dockerfile,
-		Platform:   "linux/amd64",
-		BuildArgs: []dagger.BuildArg{
-			{Name: "LDFLAGS", Value: ldflags},
-		},
-	})
-	arm64 := t.Source.DockerBuild(dagger.DirectoryDockerBuildOpts{
-		Dockerfile: dockerfile,
-		Platform:   "linux/arm64",
-		BuildArgs: []dagger.BuildArg{
-			{Name: "LDFLAGS", Value: ldflags},
-		},
-	})
+	images := t.BuildImages(ctx, version, commit)
 
 	for _, tag := range tags {
 		ref := fmt.Sprintf("%s/%s:%s", registry, imageName, tag)
 		addr, err := dag.Container().
 			Publish(ctx, ref, dagger.ContainerPublishOpts{
-				PlatformVariants: []*dagger.Container{amd64, arm64},
+				PlatformVariants: images,
 			})
 		if err != nil {
 			return published, fmt.Errorf("failed to publish %s: %w", ref, err)
